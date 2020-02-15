aa206e63add7c7296d4f97fa0ccadc8f4d223d5d
Eric Xiao <erixrekt69@gmail.com>
2020-02-15 10:44:29 -0700

    Edits

--- Contracts/contracts/contract.sol
+++ Contracts/contracts/contract.sol
@@ -3,9 +3,10 @@ pragma solidity >=0.4.22 <0.7.0;
 contract Contract {
     uint public a = 123;
 
+    event updatedValue(uint val, address caller);
+
     function setValue(uint newVal) external {
         a = newVal;
+        emit updatedValue(newVal, msg.sender);
     }
-
-    //function returnValue returns ()
 }
--- test/index.js
+++ test/index.js
@@ -1,13 +1,21 @@
 const web3 = require('web3');
 const metadata = require('../Contracts/build\\contracts/Contract.json');
 const abi = metadata.abi;
-//console.log(JSON.stringify(abi));
 
-web3js = new web3(new web3.providers.HttpProvider("http://localhost:7545"));
+web3js = new web3(new web3.providers.WebsocketProvider("ws://localhost:8545"));
 web3js.eth.getBlockNumber().then(console.log);
 
-const address = metadata.networks[5777].address;
-//console.log(address);
+const address = "0x846aC8F497a5A7569077655C61C521F0D5545508";
+
 const myContract = new web3js.eth.Contract(abi, address);
 myContract.methods.a().call()
 .then(console.log);
+myContract.methods.setValue(43223).send({
+    "from": "0xfc2077CA7F403cBECA41B1B0F62D91B5EA631B5E"
+});
+
+myContract.getPastEvents("updatedValue", {
+    "fromBlock": 0
+}, function (error, event) { 
+    console.log(" ondata ", event[0].returnValues.val);
+});

e4fa37c3a55995e082ef23f65e69ef3b63a2c015
Ermano Nicolas Joseph <jnermano86@gmail.com>
2020-02-15 15:08:17 -0700

    - code is more modular - Can emit signal - everithing is happening in matchMaker.js

--- backend/src/matchMaker.js
+++ backend/src/matchMaker.js
@@ -1,2 +1,17 @@
-require('./networkCatcher.js');
-require('./chainCatcher.js');
+const networkCatcher = require('./networkCatcher.js');
+const chainCatcher = require('./chainCatcher.js');
+const catcherApi = require("./catcherApi.js");
+
+let netData = [];
+let trxData = [];
+
+networkCatcher.on("network", (data) => {
+    console.log(data);
+});
+
+chainCatcher.on("new_trx", (trx) => {
+    console.log(trx);
+});
+
+
+

7ea8b1e16d88da51c4e8ff6d67c6cbe716df69ee
Eric Xiao <erixrekt69@gmail.com>
2020-02-15 15:03:46 -0700

    Made etherscan links

--- extension/js/main.js
+++ extension/js/main.js
@@ -1,31 +1,60 @@
-const data = require("../data.json"); //JSON file with data
+var data = [
+    {
+        "macAddress": "580293423",
+        "time": "2/15/2020 18:10",
+        "hash": "0x004kc32",
+        "URL": "tornado.cash"
+    },
+    {
+        "macAddress": "5844393423",
+        "time": "2/15/2020 20:10",
+        "hash": "0x4390004kc32",
+        "URL": "tornado.cash"
+    }
+]
 
-function printTable() {
-    data.forEach(block => {
+function printTable(d) { //Once required JSON file, pass variable instance of it to printTable
+    d.forEach(block => {
         //Initializing variable for creating HTML elements
         var tr = document.createElement("TR");
 
         var macAddress = document.createElement("TD");
         macAddress.setAttribute("id", "macAddress");
-        var addressText = document.createTextNode(block.macAddress);
-        macAddress.appendChild(addressText);
+        var a1 = document.createElement("A");
+        a1.setAttribute("href", "https://etherscan.io/tx/" + block["hash"]);
+        a1.setAttribute("target", "_blank");
+        var addressText = document.createTextNode(block["macAddress"]);
+        a1.appendChild(addressText);
+        macAddress.appendChild(a1);
 
         var time = document.createElement("TD");
         time.setAttribute("id", "time");
-        var timeText = document.createTextNode(block.time);
-        time.appendChild(timeText);
+        var a2 = document.createElement("A");
+        a2.setAttribute("href", "https://etherscan.io/tx/" + block["hash"]);
+        a2.setAttribute("target", "_blank");
+        var timeText = document.createTextNode(block["time"]);
+        a2.appendChild(timeText);
+        time.appendChild(a2);
 
         var hash = document.createElement("TD");
         hash.setAttribute("id", "hash");
-        var hashText = document.createTextNode(block.hash);
-        hash.appendChild(hashText);
+        var a3 = document.createElement("A");
+        a3.setAttribute("href", "https://etherscan.io/tx/" + block["hash"]);
+        a3.setAttribute("target", "_blank");
+        var hashText = document.createTextNode(block["hash"]);
+        a3.appendChild(hashText);
+        hash.appendChild(a3);
 
         var URL = document.createElement("TD");
         URL.setAttribute("id", "URL");
-        var URLText = document.createTextNode(block.URL);
-        URL.appendChild(URLText);
+        var a4 = document.createElement("A");
+        a4.setAttribute("href", "https://etherscan.io/tx/" + block["hash"]);
+        a4.setAttribute("target", "_blank");
+        var URLText = document.createTextNode(block["URL"]);
+        a4.appendChild(URLText);
+        URL.appendChild(a4);
 
-        var table = document.getElementById("table");
+        var table = document.getElementById("mainTable");
         tr.appendChild(macAddress);
         tr.appendChild(time);
         tr.appendChild(hash);
@@ -34,4 +63,7 @@ function printTable() {
     })
 }
 
-document.addEventListener("DOMContentLoaded", printTable);
+
+window.onload = function() {
+    printTable(data);
+}

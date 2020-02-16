7d2cdf23a3bab4bc37b3046a3bf2de8cda153206
SCBuergel <s.buergel@gmail.com>
2020-02-15 19:39:48 -0700

    🐛 matched events sent via websocket to client

--- backend/src/catcherApi.js
+++ backend/src/catcherApi.js
@@ -3,40 +3,40 @@ const WebSocket = require("ws");
 // local dependencies
 const matchMaker = require("./matchMaker.js");
 
-matchMaker.on("newMatch", (matchData) => {
-  console.log("NEW MATCH: " + JSON.stringify(matchData, null, 2));
+
+const wss = new WebSocket.Server({port: 3031});
+
+wss.on("connection", function connection(ws) {
+    console.log("WebSocket is online on port: 3031");
+
+    ws.send(JSON.stringify({
+        msg: "Hello World"
+    }));
+
+    matchMaker.on("newMatch", (matchData) => {
+      console.log("NEW MATCH: " + JSON.stringify(matchData, null, 2));
+      ws.send(JSON.stringify(matchData));
+    });
+
+  ws.on("message", function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
 });
 
-class CatcherAPI {
 
-    constructor() {
-        this.wss = new WebSocket.Server({port: 3031});
 
-        this.wss.on("connection", function connection(ws) {
-            console.log("WebSocket is online on port: 3030");
-        
-            ws.send(JSON.stringify({
-                msg: "Hello World"
-            }));
-        
-          ws.on("message", function incoming(data) {
-            wss.clients.forEach(function each(client) {
-              if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data);
-              }
-            });
-          });
-        });
-        
-    }
+// class CatcherAPI {
+//     // send(data) {
+//     //     if(this.wss)
+//     //         return this.wss.server.send(JSON.stringify(data));
+//     //     else
+//     //         return null;
+//     // }
+// }
 
-    send(data) {
-        if(this.wss)
-            return this.wss.server.send(JSON.stringify(data));
-        else
-            return null;
-    }
-}
-
-const catcherApi = new CatcherAPI();
-module.exports = catcherApi;
+// const catcherApi = new CatcherAPI();
+// module.exports = catcherApi;

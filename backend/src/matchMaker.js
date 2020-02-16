d47728aee51248af176c3d3938d6d0c5f453b2b4
Ermano Nicolas Joseph <jnermano86@gmail.com>
2020-02-15 21:54:29 -0700

    starting with matching

--- backend/src/matchMaker.js
+++ backend/src/matchMaker.js
@@ -14,7 +14,7 @@ let txs = [];
 
 networkCatcher.on("newPacket", (packet) => {
     packets.push(packet);
-    match();
+    // match();
 });
 
 chainCatcher.on("newTx", (tx) => {
@@ -31,12 +31,45 @@ function match() {
     pIndex = packets.length - 1;
     tIndex = txs.length - 1;
 
-    matchMaker.emit("newMatch", {
-        MAC: packets[pIndex].MAC,
-        time: txs[tIndex].blockTime,
-        hash: txs[tIndex].txHash,
-        URL: packets[pIndex].URL
-    });
+    const tx = txs[txs.length - 1];
+
+    const targetDomain = ".com";
+    const deltaTimeS = 100;
+
+    // find all packets of target domain
+    let targetPackets = [];
+    console.log("going to process another match...");
+    packets.forEach((packet) => {
+        let url = packet.URL;
+        if(packet.URL.includes(targetDomain)) {
+            // we found a packet for the target domain!
+            // now check if there are any transactions within +/- deltaTimeS
+            // which match the dapp
+
+            //console.log("found packet: " + JSON.stringify(packet));
+            const txTime = tx.blockTime;
+            const packetTime = packet.time / 1000;
+            const dt = Math.abs(txTime - packetTime);
+            //console.log("txTime: " + txTime + ", packetTime: " + packetTime + ", dt: " + dt);
+
+            if(dt < deltaTimeS) {
+                console.log("MATCH");
+                matchMaker.emit("newMatch", {
+                    MAC: packet.MAC,
+                    time: tx.blockTime,
+                    hash: tx.txHash,
+                    URL: packet.URL
+                });
+
+                // in order to prevent multiple results, remove the tx and return
+                txs.pop();
+                return;
+            }
+        }
+    })
+
+
+
 }
 
 module.exports = matchMaker;
